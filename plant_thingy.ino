e3d22d1f9ef95731ea894d1b0adf9a8b03752e55
Raghav <97241429+Stupid-Creations@users.noreply.github.com>
2024-08-27 21:03:54 +0530

    Update plant_thingy.ino
    add DHT 22 measurements

--- plant_thingy.ino
+++ plant_thingy.ino
@@ -1,14 +1,20 @@
 #include <LiquidCrystal.h>
+#include <SimpleDHT.h>
 
 LiquidCrystal lcd(11, 10, 9, 8, 7,6);
 
 const int R = 1;
 const int G = 2;
-const int photoresistor = 5;
-const int pins[] = {16,17,18,19,20,21,22,26,27,28};
+const int DHT  = 12;
+const int photoresistor = 27;
+const int pins[] = {16,17,18,19,20,21,22,26,5,28};
+//28,5,26,22,21,20,19,18,17,16
+SimpleDHT22 dht22(DHT);
+
+
 
 void setup() {
-  Serial.begin(115200);
+  Serial1.begin(115200);
   lcd.begin(16, 2);
   //lcd.print("Hello World!");
   pinMode(R, OUTPUT);
@@ -26,21 +32,37 @@ void loop() {
   lcd.clear();
   lcd.setCursor(0,0);
 
-  float brightness = map(analogRead(photoresistor),0,1024,0,10);
+  float temperature = 0;
+  float humidity = 0;
 
+  int err = SimpleDHTErrSuccess;
+
+  float bright = analogRead(photoresistor);
+  float brightness = map(bright,8,1016,0,10);
+
+  Serial1.println(brightness);
   for(int i = 0; i < 10;i++){
-    if(i < brightness){
+    if(i<brightness){
       digitalWrite(pins[i],HIGH);
     }else{
       digitalWrite(pins[i],LOW);
     }
   }
+  
 
-
+  if ((err = dht22.read2(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    lcd.print("oops, DHT fail"); lcd.print(SimpleDHTErrCode(err));
+    lcd.print(","); lcd.println(SimpleDHTErrDuration(err)); delay(2000);
+    return;
+  }
   lcd.clear();
-  lcd.print(analogRead(photoresistor)); lcd.print("*C, ");
-
-
+  lcd.print((temperature*(9/5))+32); lcd.print("F, ");
+  lcd.print((float)humidity); lcd.println("%");
+  if(humidity < 20){
+    lcd.setCursor(0,1);
+    lcd.print("waterr :(");
+    okay = false;
+  }
   if(okay == false){
     analogWrite(R,255);
     analogWrite(G,0);
@@ -50,5 +72,7 @@ void loop() {
         analogWrite(R,0);
 
   }
+  // DHT22 sampling rate is 0.5HZ.
+  delay(2500);
 
 }
